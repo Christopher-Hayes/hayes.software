a83b293652f1b7e9410a1d7ceba4637b8ef56a68
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-12-02 09:53:02 -0500

    :art: Updated header; Minor perf optimizations

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 import './styles/main.css';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+//import Alpine from 'alpinejs'
+//import collapse from '@alpinejs/collapse'
 
 const env = document.querySelector('body').dataset.env;
+
+const loadAlpine = async () => {
+  window.Alpine = (await import('alpinejs')).default;
+  const collapse = (await import('@alpinejs/collapse')).default;
+
+  window.Alpine.plugin(collapse);
+  window.Alpine.start();
+};
 
 // Check that service workers are supported
 if ('serviceWorker' in navigator && env === 'production') {
   // use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
+  window.addEventListener('DOMContentLoaded', async () => {
+    loadAlpine();
     try {
       navigator.serviceWorker.register('/sw.js');
     } catch (error) {
       console.error('Service worker registration failed: ', error);
     }
+  });
+} else {
+  window.addEventListener('DOMContentLoaded', async () => {
+    loadAlpine();
   });
 }
